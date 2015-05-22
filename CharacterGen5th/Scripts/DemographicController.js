f11647b947e6b606489ed174646189c4995c5486
Ttiamus <Ttiamus@uah.edu>
2015-05-21 23:01:17 -0500

    Changed demographic view to use angular instead of razor

---
+++ CharacterGen5th/Scripts/DemographicController.js
@@ -0,0 +1,35 @@
+﻿var demographic = angular.module('demographic', []);
+
+demographic.controller('demographicController', function ($scope, $http)
+{
+    $scope.strScore = "10";
+
+    $scope.genders =
+    [
+        "Male",
+        "female"
+    ];
+    $scope.gender = $scope.genders[0];
+
+    $scope.races =
+    [//Make http call to database to return race and other 
+        "Dwarf",
+        "Dragonborn"
+    ];
+    $scope.race = $scope.races[0];
+
+    $scope.alignments =
+    [
+        "Lawful Good",
+        "Lawful Neutral"
+    ];
+    $scope.alignment = $scope.alignments[0];
+
+    $scope.gods =
+    [
+        "Auril",
+        "Sarenrae"
+    ];
+    $scope.god = $scope.gods[0];
+
+});
