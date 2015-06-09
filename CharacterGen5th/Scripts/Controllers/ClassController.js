04bd0954d51627708a78915c6bab459c7c9d93e6
Ttiamus <Ttiamus@uah.edu>
2015-06-08 22:55:26 -0500

    Got the hit points working thanks to mike's idea

--- CharacterGen5th/Scripts/Controllers/ClassController.js
+++ CharacterGen5th/Scripts/Controllers/ClassController.js
@@ -2,63 +2,59 @@
 
 Class.controller('classController', function ($scope, $http)
 {
-    $scope.getTimes = function ()
+    $scope.evalHitPoints = function()
     {
-        var arr = new Array( 1, 2, 3, 4, 5,
-                            6, 7, 8, 9, 10,
-                            11, 12, 13, 14, 15,
-                            16, 17, 18, 19, 20 );
-        return arr;
-    };
+        $scope.hitPointTotal = 0;
 
+        angular.forEach($scope.classLevels, function (level) {
+            $scope.hitPointTotal = parseInt($scope.hitPointTotal, 10) + (parseInt(level.hitPoints, 10) || 0);
+        });
+    }
+
+    
     var init = function() {
         $http.get("/CharacterGen/CharacterGen/ClassInit")
-            .success(function(data) {
-                
+            .success(function (data) {
+
                 $scope.classes = [];
-               /* $scope.classes.push(
+                $scope.classes.push(
                 {
-                    id: 0,
-                    name: "Choose A Class",
-                    hitDie: ""
-                });
-                */
+                    Name: "Choose a Class"
+                })
+
                 for (var i = 0; i < data.length; i++)
                 {
                     $scope.classes.push(data[i]);
                 }
                 $scope.class = $scope.classes[0];
 
-                $scope.classSelection =
-                [
-                    { id: 1, name: "", hitPoints: 0 },
-                    { id: 2, name: "", hitPoints: 0 },
-                    { id: 3, name: "", hitPoints: 0 },
-                    { id: 4, name: "", hitPoints: 0 },
-                    { id: 5, name: "", hitPoints: 0 },
-                    { id: 6, name: "", hitPoints: 0 },
-                    { id: 7, name: "", hitPoints: 0 },
-                    { id: 8, name: "", hitPoints: 0 },
-                    { id: 9, name: "", hitPoints: 0 },
-                    { id: 10, name: "", hitPoints: 0 },
-                    { id: 11, name: "", hitPoints: 0 },
-                    { id: 12, name: "", hitPoints: 0 },
-                    { id: 13, name: "", hitPoints: 0 },
-                    { id: 14, name: "", hitPoints: 0 },
-                    { id: 15, name: "", hitPoints: 0 },
-                    { id: 16, name: "", hitPoints: 0 },
-                    { id: 17, name: "", hitPoints: 0 },
-                    { id: 18, name: "", hitPoints: 0 },
-                    { id: 19, name: "", hitPoints: 0 },
-                    { id: 20, name: "", hitPoints: 0 },
-                ];
+                $scope.classLevels =
+                    [
+                        { id: 1, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 2, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 3, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 4, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 5, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 6, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 7, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 8, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 9, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 10, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 11, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 12, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 13, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 14, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 15, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 16, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 17, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 18, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 19, classSelected: $scope.classes[0], hitPoints: "" },
+                        { id: 20, classSelected: $scope.classes[0], hitPoints: "" },
+                    ];
 
-                $scope.hitPointTotal =
-                    $scope.classSelection[0].hitPoints + $scope.classSelection[1].hitPoints + $scope.classSelection[2].hitPoints + $scope.classSelection[3].hitPoints + $scope.classSelection[4].hitPoints +
-                    $scope.classSelection[5].hitPoints + $scope.classSelection[6].hitPoints + $scope.classSelection[7].hitPoints + $scope.classSelection[8].hitPoints + $scope.classSelection[9].hitPoints +
-                    $scope.classSelection[10].hitPoints + $scope.classSelection[11].hitPoints + $scope.classSelection[12].hitPoints + $scope.classSelection[13].hitPoints + $scope.classSelection[14].hitPoints +
-                    $scope.classSelection[15].hitPoints + $scope.classSelection[16].hitPoints + $scope.classSelection[17].hitPoints + $scope.classSelection[18].hitPoints + $scope.classSelection[19].hitPoints;
-            } );
+                $scope.evalHitPoints();
+            });
     }
+
     init();
 });
